1966ffc3a17c351e0dc6e896f77f59ce185c2fb9
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-02 16:43:29 -0400

    set up friend data array, set up frontend designs for html pages

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//Data for each person signed up
+var friendArray = [
+{
+    "name":"Smudge",
+    "photo":"https://i.kym-cdn.com/photos/images/newsfeed/001/505/718/136.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+
+// export the array so it is accessible to other files using require.
+module.exports = friendArray;
