3a354b6cd5f8320df2afb2b8a0666768485c41ab
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-11-09 09:48:32 -0500

    process.env.PORT on server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 //set up express app
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //allows for data parsing
 app.use(express.urlencoded({ extended: true }));
